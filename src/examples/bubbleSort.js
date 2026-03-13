2a013cea15410656313b64ad17ad0507589749f6
Roman4k-gg <sendelroma@gmail.com>
2026-03-13 23:25:09 +0700

    AcId

--- src/examples/bubbleSort.js
+++ src/examples/bubbleSort.js
@@ -22,7 +22,7 @@ export function getBubbleSortBlocks() {
     });
   }
 
-  const arrayNode = createNode("array", { name: "a", length: "6", elements: "5, 2, 8, 1, 9" }, 100, 50);
+  const arrayNode = createNode("array", { name: "a", length: "6", elements: "5, 2, 8, 1, 9, 0" }, 100, 50);
   const nNode = createNode("assign", { target: "n", value: "5" }, 100, 150);
   const forI = createNode("forBl", { variable: "i=0", endCondition: "i<n-1", step: "i+=1" }, 100, 250);
   const forJ = createNode("forBl", { variable: "j=0", endCondition: "j<n-i-1", step: "j+=1" }, 100, 350);
@@ -30,11 +30,8 @@ export function getBubbleSortBlocks() {
   const tempAssign = createNode("assign", { target: "temp", value: "a[j]" }, 400, 450);
   const swap1 = createNode("assign", { target: "a[j]", value: "a[j+1]" }, 400, 550);
   const swap2 = createNode("assign", { target: "a[j+1]", value: "temp" }, 400, 650);
-  const out0 = createNode("cout", { target: "a[0]" }, 100, 750);
-  const out1 = createNode("cout", { target: "a[1]" }, 100, 850);
-  const out2 = createNode("cout", { target: "a[2]" }, 100, 950);
-  const out3 = createNode("cout", { target: "a[3]" }, 100, 1050);
-  const out4 = createNode("cout", { target: "a[4]" }, 100, 1150);
+  const out0 = createNode("cout", { target: "a" }, 100, 750);
+
 
   connect(arrayNode.id, nNode.id);
   connect(nNode.id, forI.id);
@@ -44,10 +41,6 @@ export function getBubbleSortBlocks() {
   connect(tempAssign.id, swap1.id);
   connect(swap1.id, swap2.id);
   connect(forI.id, out0.id, "exit");
-  connect(out0.id, out1.id);
-  connect(out1.id, out2.id);
-  connect(out2.id, out3.id);
-  connect(out3.id, out4.id);
 
   return { nodes, edges };
 }
